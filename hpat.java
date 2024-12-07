1ca3fe8e83d65e540ac43dc9695f1c7a79c0a0f3
manisha4450 <chowdaryc226@gmail.com>
2024-12-07 15:05:26 +0530

    Add files via upload

---
+++ hpat.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hpat{
+    public static void main(String[] args){
+        int n=10;
+        n-=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || i==n/2 || j==n-1)
+                System.out.print("*"+" ");
+                else System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+}
