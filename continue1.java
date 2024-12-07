4efffffd2a4eead83ecc691dde239457dd159f7f
manisha4450 <chowdaryc226@gmail.com>
2024-12-07 11:50:10 +0530

    Add files via upload

---
+++ continue1.java
@@ -0,0 +1,11 @@
+public class continue1{
+    public static void main(String[] args){
+        System.out.println("continue example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            continue;
+            System.out.println(i);
+        }
+    }
+}
