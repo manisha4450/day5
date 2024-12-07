4ba380fdda1287703aa33d24bbffcb9f47f183b1
manisha4450 <chowdaryc226@gmail.com>
2024-12-07 11:40:53 +0530

    Add files via upload

---
+++ butterfly1.java
@@ -0,0 +1,16 @@
+public class butterfly1{
+    public static void main(String[] args){
+        int n=10;
+        n-=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || j==n-1 || i+j==n-1 || i==j || i==n/2)
+                System.out.print("*"+" ");
+                else System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+}
